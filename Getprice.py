b1bcb178dcc5a339d5cc11220fe0192753ed5119
SungSoo KIM <sdasd127@gmail.com>
2017-11-13 08:09:49 +0000

    edit

---
+++ Getprice.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pandas_datareader.data as web
+import datetime
+import json
+import requests
+
+
+def get_realtime(code):
+    rsp = requests.get('https://finance.google.com/finance?q='+code+'&output=json')
+    print(rsp)
+    if rsp.status_code in (200,):
+        fin_data = json.loads(rsp.content[6:-2].decode('unicode_escape'))
+        rt_price = fin_data['l']
+
+    return rt_price
+    
