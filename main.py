13a9c32c845789ea62d668ac0001fe793db71a1d
SungSoo KIM <sdasd127@gmail.com>
2017-11-13 08:03:07 +0000

    make it better

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ except ImportError:
         os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
     )
     import apiai
-CLIENT_ACCESS_TOKEN = '1885ee7d899e40b6b3ab98bee443a2cb'
+CLIENT_ACCESS_TOKEN = 'f6e72afa001444d18c4fceeb9061b7f7'
 ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
 # Flask app should start in global layout
@@ -32,7 +32,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1> 카페 뿌리입니다! ! </h1>"
+    return "<h1> This is Quantec </h1>"
 
 @app.route('/keyboard')
 def Keyboard():
@@ -81,111 +81,7 @@ def makeWebhookresult(req):
             # "contextOut": [],
             "source": "apiai-onlinestore-shipping"
         }
-    elif req.get("result").get("action") == "order.noncoffee":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        print(parameters)
         
-        if parameters.get('non_coffee')!="":
-            product = parameters.get("non_coffee")
-            cost = getcost.get_cost("non_coffee",product)
-        if parameters.get("Hot") =='ICE':
-            cost += 500
-        if parameters.get("takeout") == 'Takeout':
-            cost -= 500
-            
-        speech = "주문하신 "+str(product)+" 의 가격은 "+str(cost)+"원 입니다."
-            
-        print("Response:")
-        print(speech)
-    
-        return {
-            "speech": speech,
-            "displayText": speech,
-            #"data": {},
-            # "contextOut": [],
-            "source": "cafe-bburi"
-        }
-    elif req.get("result").get("action") == "order.bakery":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        print(parameters)
-        
-        if parameters.get('bakery')!="":
-            product = parameters.get("bakery")
-            cost = getcost.get_cost("bakery",product)
-        speech = "주문하신 "+str(product)+" 의 가격은 "+str(cost)+"원 입니다."
-            
-        print("Response:")
-        print(speech)
-    
-        return {
-            "speech": speech,
-            "displayText": speech,
-            #"data": {},
-            # "contextOut": [],
-            "source": "cafe-bburi"
-        }  
-        
-        
-    elif req.get("result").get("action") == "callMenu":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        category = parameters.get("Menu")
-        Menu = getcost.get_menu(category)
-   
-        speech = str(category)+" 메뉴는 "+"/ ".join(Menu)+" 가 있습니다.\n"
-        print("Response:")
-        print(speech)
-    
-        return {
-            "speech": speech,
-            "displayText": speech,
-            #"data": {},
-            # "contextOut": [],
-            "source": "cafe-bburi"
-        }
-        
-    elif req.get("result").get("action") == "getprice":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        category = parameters.get("product")
-        price = getcost.get_price(category)
-   
-        speech = str(category)+"의 가격은 "+str(price)+"원 입니다."
-        print("Response:")
-        print(speech)
-    
-        return {
-            "speech": speech,
-            "displayText": speech,
-            #"data": {},
-            # "contextOut": [],
-            "source": "cafe-bburi"
-        }
-    else:
-        return {}
-        
-def makeWebhookmenu(req):
-    if req.get("result").get("action") != "call.menu" :
-        return {}
-        
-    result = req.get("result")
-    parameters = result.get("parameters")
-    category = parameters.get("Menu")
-    Menu = getcost.get_menu(category)
-   
-    speech = str(category)+" 메뉴는 "+" /".join(str(Menu))+" 가 있습니다.\n"
-    print("Response:")
-    print(speech)
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        # "contextOut": [],
-        "source": "cafe-bburi"
-    }
         
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
