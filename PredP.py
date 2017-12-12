00ff25126bc0e3dc3799d23e22725388cf6ee829
SungSoo KIM <sdasd127@gmail.com>
2017-12-12 03:10:13 +0000

    edit

--- PredP.py
+++ PredP.py
@@ -2,13 +2,13 @@
 import pandas as pd
 
 def get_future(product):
-    tmp = pd.read_csv("pred.csv",dtype=str,encoding='utf-8')
+    tmp = pd.read_csv("pred.csv",dtype=str,encoding='utf-8',header=None)
     idx = 0
     for i in range(len(tmp)):
-        if(product == tmp['회사명'][i]):
+        if(product == tmp[0][i]):
             idx = i
             break
-    price = tmp['예상가격'][idx]
+    price = tmp[1][idx]
     print(type(price))
     price = int(float(price))
     return price
